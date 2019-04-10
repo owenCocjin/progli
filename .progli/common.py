42f2be80bad974903b46d1498d77541a456d3c46
owenCocjin <owen_jamescf@hotmail.com>
2019-04-09 22:26:31 -0400

    Added header info

--- .progli/common.py
+++ .progli/common.py
@@ -1,9 +1,16 @@
+## Author:	Owen Cocjin
+## Version:	0.1
+## Date:	09/04/19
+
 '''---------------------+
 |		VARIABLES		|
 +---------------------'''
 global plPath
 plPath="./.progli/program_list.test"
 
+'''---------------------+
+|		FUNCTIONS		|
++---------------------'''
 def error(location, message="Unknown Error", code=1):
 	print("\033[31m[\033[34m|\033[31mX]\033[0m({}): {}".format(location, message))
 	exit(code)
