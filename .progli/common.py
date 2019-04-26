83d67edcf26dab3ddeb6c6f189a8929969ca2a65
owenCocjin <owen_jamescf@hotmail.com>
2019-04-26 12:54:40 -0400

    Added dope plate comments

--- .progli/common.py
+++ .progli/common.py
@@ -4,15 +4,15 @@
 
 import os
 
-'''---------------------+
-|		VARIABLES		|
-+---------------------'''
+'''-----------------------+
+|        VARIABLES        |
++-----------------------'''
 global plPath
 plPath="{}/program_list".format(os.path.dirname(os.path.realpath(__file__)))
 
-'''---------------------+
-|		FUNCTIONS		|
-+---------------------'''
+'''-----------------------+
+|        FUNCTIONS        |
++-----------------------'''
 def error(location, message="Unknown Error", code=1):
 	print("\033[31m[\033[34m|\033[31mX]\033[0m({}): {}".format(location, message))
 	exit(code)
